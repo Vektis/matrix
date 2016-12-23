02b03edda615b1e4ff28576be0095f18f9ec9841
vianrosal <pbgaming2225@gmail.com>
2016-12-22 22:19:42 -0500

    well

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,12 @@ public class Main {
 	// write your code here
         int a[][] = {{1,2,-2,0},{-3,4,7,2},{6,0,3,1}};
         int b[][] = {{-1,3},{0,9},{1,-11},{4,-5}};
-        System.out.println(MatrixMult.mult(a,b));
+        int v[][] = MatrixMult.mult(a,b);
+        for(int i[]:v){
+            for(int k:i){
+                System.out.print(k+ " ");
+            }
+            System.out.println(" ");
+        }
     }
 }
--- src/com/company/MatrixMult.java
+++ src/com/company/MatrixMult.java
@@ -7,20 +7,25 @@ public class MatrixMult {
     public MatrixMult(){}
     public static int[][] mult (int a[][],int b[][]) {
         int x = 0;
+        int xc = 0;
+        int t = 0;
         int c[][] = new int[a.length][b[0].length];
-        for (int i = 0; i < a[i].length; i++) {
+
+        for (int i = 0; i < a.length; i++) {
             int o = 0;
-            for (int j = 0; j < a[i].length; j++) {
+            for (int j = 0; j < b[i].length; j++) {
                 //System.out.print(a[i][j]+" ");
                // System.out.print(b[j][i]+" ");
-                x += a[i][j] * b[j][i];
-                System.out.println(a[i][j]);
+                for (int k = 0; k < a[0].length; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
             }
             //System.out.println(x);
 
             x = 0;
 
         }
+
         return c;
     }
 
